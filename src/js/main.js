45ac49c785bb12f50a46c5c4b20ee745cae79af7
Énio Sousa <eniofcup@hotmail.com>
2020-10-16 21:28:55 +0100

    Completei a folha de instruções. Alterei a maneira de como se guarda a a pagina currente, será posteriormente guardado numa pilha. Adicionei uma funcionalidade de voltar ao painel anterior

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,21 @@
  * 
  * 0 - Authentication
  * 1 - Configuration
+ * 2 - Instructions 
+ *      3 - Game Play
+ *      4 - Valid Moves
+ *      5 - End Game
  */
 
+/* I have changed the way we keep track of the current page
+    in order for us to go back to the previous pages.
+    I think its a usefull function and its going to show
+    its "weight" in the instruction manual and score page*/
+var memPanel = [0];
 
-// STATE
-var curPanel = 0;
+// Instruction manual have several pages, but i dont want to have it in memory(memPanel)
+var curInst = 3;
+
 const configuration = {
     gameType: "ai",
     playerColor: "light",
@@ -17,6 +27,7 @@ const configuration = {
 window.onload = function () {
     setupAuthentication();
     setupConfiguration();
+    setupInstruction();
 }
 
 function setupAuthentication() {
@@ -50,13 +61,78 @@ function setupConfiguration() {
 
         configuration.aiDifficulty = document.getElementById("ai-difficulty").value;
 
-        switchPanel(2);
+        switchPanel(999);
     }
 }
 
+function setupInstruction(){
+    let instButton = document.getElementById("inst-button");
+    let nextButton = document.getElementById("inst-next");
+    let prevButton = document.getElementById("inst-prev");
+
+    instButton.onclick = function (){
+        switchPanel(2);
+
+        curInst = 3;
+        showPanel(curInst);
+
+        for( let i = 4; i < 6; i++ )
+            hidePanel(i);
+    }
+
+    nextButton.onclick = function (){
+        nextInst();
+    }
+
+    prevButton.onclick = function(){
+        prevInst();
+    }
+
+}
+
+function curPanel(){
+    return memPanel[memPanel.length-1];
+}
+
 function switchPanel(newPanel) {
-    hidePanel(curPanel);
+    hidePanel(curPanel());
     showPanel(newPanel);
+    memPanel.push(newPanel);
+}
+
+function switchPanelBack(){
+    hidePanel(memPanel.pop());
+    showPanel(curPanel());
+}
+
+/* Functions to traverse instruction manual. Next instruction page
+    In case there is no next page we go back to the previous panel*/
+function nextInst(){
+    if ( curInst<5 ){
+        hidePanel(curInst);
+        curInst+=1;
+        showPanel(curInst);
+    }  
+
+    else{
+        curInst = 3;
+        switchPanelBack();
+    }
+}
+
+/* Previous instruction page. In case there is bo previous page
+    we go to the previous panel*/
+function prevInst(){
+    if ( curInst < 3 ){
+        switchPanelBack();
+        curInst = 3;
+    }
+
+    else{
+        hidePanel(curInst);
+        curInst-=1;
+        showPanel(curInst);
+    }
 }
 
 function hidePanel(panel) {
@@ -75,7 +151,15 @@ function getBoxName(panel) {
             return "ident-box";
         case 1:
             return "config-box";
+        case 2:  
+            return "inst-box";
+        case 3:
+            return "inst-box-1";
+        case 4:
+            return "inst-box-2";
+        case 5:
+            return "inst-box-3";
     }
 
-    return "";
+    return 0;
 }
