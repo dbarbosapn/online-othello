289d89a465feba9eba95c0ff93dde6ec5304f719
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2020-10-17 12:25:13 +0100

    Implements modals and fixes styling

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,13 @@
  * 
  * 0 - Authentication
  * 1 - Configuration
- * 2 - Instructions 
- *      3 - Game Play
- *      4 - Valid Moves
- *      5 - End Game
- * 6 - HighScore
+ * 2 - Game
+ * 3 - Instructions
+ * 4 - Highscore
  */
+var currPanel = 0;
 
-var memPanel = [0];
-
-var curInst = 3;
+var currInstructionBox = 1;
 
 const configuration = {
     gameType: "ai",
@@ -21,10 +18,11 @@ const configuration = {
 }
 
 window.onload = function() {
+    hideOverlay();
     setupAuthentication();
     setupConfiguration();
     setupInstructions();
-    setupHighScore(); 
+    setupHighScore();
 }
 
 function setupAuthentication() {
@@ -59,128 +57,106 @@ function setupConfiguration() {
 
         configuration.aiDifficulty = document.getElementById("ai-difficulty").value;
 
-        switchPanel(999);
+        switchPanel(2);
     }
-}
-
-function resetInstPage() {
-
-    curInst = 3;
-    showPanel(curInst);
-
-    for (let i = 4; i < 6; i++)
-        hidePanel(i);
-
 }
 
 function setupInstructions() {
     let instButton = document.getElementById("inst-button");
     let nextButton = document.getElementById("inst-next");
     let prevButton = document.getElementById("inst-prev");
+    let closeButton = document.getElementById("close-button-inst");
+    prevButton.style.display = "none";
+
+    let instBox1 = document.getElementById("inst-box-1");
+    let instBox2 = document.getElementById("inst-box-2");
+    instBox2.style.display = "none";
+    let instBox3 = document.getElementById("inst-box-3");
+    instBox3.style.display = "none";
 
     instButton.onclick = function() {
-        switchPanel(2);
+        showPanel(3, true);
     }
 
     nextButton.onclick = function() {
-        nextInst();
+        if (currInstructionBox == 1) {
+            instBox1.style.display = "none";
+            instBox2.style.display = "flex";
+            prevButton.style.display = "block";
+        } else {
+            instBox2.style.display = "none";
+            instBox3.style.display = "flex";
+            nextButton.style.display = "none";
+        }
+        currInstructionBox++;
     }
 
     prevButton.onclick = function() {
-        prevInst();
+        if (currInstructionBox == 2) {
+            instBox2.style.display = "none";
+            instBox1.style.display = "flex";
+            prevButton.style.display = "none";
+        } else {
+            instBox3.style.display = "none";
+            instBox2.style.display = "flex";
+            nextButton.style.display = "block";
+        }
+        currInstructionBox--;
     }
 
+    closeButton.onclick = function() {
+        currInstructionBox = 1;
+        instBox1.style.display = "flex";
+        instBox2.style.display = "none";
+        instBox3.style.display = "none";
+        hidePanel(3, true);
+    }
 }
 
 function setupHighScore() {
-    let backButton = document.getElementById("back-button-hs");
+    let closeButton = document.getElementById("close-button-hs");
     let highScore = document.getElementById("highScore-button");
-    let instButton = document.getElementById("inst-button-hs");
 
     displayHighScore();
 
     highScore.onclick = function() {
-        switchPanel(6);
+        showPanel(4, true);
     }
 
-    backButton.onclick = function() {
-        switchPanelBack();
-    }
-
-    instButton.onclick = function() {
-        switchPanel(2);
+    closeButton.onclick = function() {
+        hidePanel(4, true);
     }
 }
 
-function curPanel() {
-    return memPanel[memPanel.length - 1];
-}
-
-/* A special case needed to be implemented when
-    switching to page 2 aka Instructions manual.
-    This happen because 3 4 and 5 are not panels 
-    but text boxes. When we load panel nº 2 we 
-    also load text box nº3, and this creates
-    a confusion when backtracking. To resolve
-    this problem, we choose not to keep track
-    of 3,4 and 5 in memPanel, instead we use the actual
-    number, meaning 4 comes after 3, 3 comes before 4 etc.. */
 function switchPanel(newPanel) {
-    if ( newPanel == 2 ) {
-        curInst = 3;
-        showPanel(curInst);
-
-        for (let i = 4; i < 6; i++)
-            hidePanel(i);
-    }
-
-    else if ( newPanel == 6 )
-        displayHighScore();
-
-    hidePanel(curPanel());
+    hidePanel(currPanel);
     showPanel(newPanel);
-    memPanel.push(newPanel);
 }
 
-function switchPanelBack() {
-    hidePanel(memPanel.pop());
-    showPanel(curPanel());
-}
-
-/* Functions to traverse instruction manual. Next instruction page
-    In case there is no next page we go back to the previous panel*/
-function nextInst() {
-    if (curInst < 5) {
-        hidePanel(curInst);
-        curInst += 1;
-        showPanel(curInst);
-    } else {
-        curInst = 3;
-        switchPanelBack();
-    }
-}
-
-/* Previous instruction page. In case there is bo previous page
-    we go to the previous panel*/
-function prevInst() {
-    if (curInst <= 3) {
-        switchPanelBack();
-        curInst = 3;
-    } else {
-        hidePanel(curInst);
-        curInst -= 1;
-        showPanel(curInst);
-    }
-}
-
-function hidePanel(panel) {
+function hidePanel(panel, modal = false) {
     let boxName = getBoxName(panel);
     document.getElementById(boxName).style.display = "none";
+
+    if (modal) {
+        hideOverlay();
+    }
 }
 
-function showPanel(panel) {
+function showPanel(panel, modal = false) {
     let boxName = getBoxName(panel);
     document.getElementById(boxName).style.display = "flex";
+
+    if (modal) {
+        showOverlay();
+    }
+}
+
+function hideOverlay() {
+    document.getElementById("dark-overlay").style.display = "none";
+}
+
+function showOverlay() {
+    document.getElementById("dark-overlay").style.display = "block";
 }
 
 function getBoxName(panel) {
@@ -190,16 +166,10 @@ function getBoxName(panel) {
         case 1:
             return "config-box";
         case 2:
-            return "inst-box";
-        /* START: Do not change this part. It will break the code */
+            return "game-box";
         case 3:
-            return "inst-box-1";
+            return "inst-box";
         case 4:
-            return "inst-box-2";
-        case 5:
-            return "inst-box-3";
-        /*END*/
-        case 6:
             return "high-score-box"
     }
 
@@ -214,29 +184,30 @@ function HighScore(name, date, score) {
 }
 
 /* Array to test high Score table writing*/
-var player = [new HighScore("Enio", "31-12-20", 69),
-            new HighScore("Jesus", "0-0-0", 1000),
-            new HighScore("Diogo", "13-7-13", -1000)];
+var players = [new HighScore("Enio", "31-12-20", 69),
+    new HighScore("Jesus", "0-0-0", 1000),
+    new HighScore("Diogo", "13-7-13", -1000)
+];
 
 /* Right now the following functions are just for
     testing the High Score panel. But i think we can
     use them when we actually want to have a real HS.
     The structure can also be usefull for implementing
-    a message board or a command feedback*/            
+    a message board or a command feedback*/
 function displayHighScore() {
     let content = "";
-    player.sort((a, b) => b.score - a.score);
-    player.forEach((player) => content += '<tr><td>' + player.name +'</td><td>' + player.date + '</td><td>' + player.score +'</td></tr>');
-    document.getElementById("highScore-content").innerHTML = content;
+    players.sort((a, b) => b.score - a.score);
+    players.forEach((player) => content += '<tr><td>' + player.name + '</td><td>' + player.date + '</td><td>' + player.score + '</td></tr>');
+    document.getElementById("highscore-content").innerHTML = content;
 }
-function miniDisplayHighScore() {
+
+function displayMiniHighScore() {
     let content = "";
-    player.sort((a, b) => a.score - b.score);
+    players.sort((a, b) => a.score - b.score);
 
-    for ( let i = 0; i<10 && i<player.length; i++ )
-        content += '<tr><td>' + player[0].name + '</td></tr>'
+    players.forEach(player => {
+        content += '<tr><td>' + player.name + '</td></tr>'
+    });
 
-    document.getElementById("mini-highScore-content").innerHTML = content;
-
+    document.getElementById("mini-highscore-content").innerHTML = content;
 }
-
