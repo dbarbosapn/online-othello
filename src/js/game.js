76523fa117cb9251fa076ce87514be9985248d5d
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2020-10-23 00:33:43 +0100

    coupled game logic with UI

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,4 @@
 class Point {
-
     constructor(x=0, y=0) {
         this.x = x;
         this.y = y;
@@ -10,44 +9,32 @@ class Point {
         return new Point(this.x + point.x, this.y + point.y);
     }
 
-    /* Sub point and return a new subed point */
-    subPoint(point) {
-        return new Point(this.x - point.x, this.y - point.y);
-    }
-
-    /* Mult point and return a the new mult point */
-    prodPoint(point) {
-        return new Point(this.x * point.x, this.y * point.y);
-    }
-
-    tostring() {
+    toString() {
         return "(" + this.x + "," + this.y+ ")";
+    }
+
+    toArrayIndex(size) {
+        return (this.y * size + this.x);
     }
 }
 
 
-/* Para poupar memoria vamos usar chars em vez de strings, estou 
-    um pouco preocupado com o uso de memoria para gerar os clones
-    '0' = empty
-    '1' = dark
-    '2' = light */
+/*  0 = empty
+    1 = dark
+    2 = light */
 class Board {
-
-    constructor(size, state = [], dark = 0, light = 0) {
+    constructor(size, state = null, dark = 0, light = 0) {
         this.dark = dark;
         this.light = light;
 
         this.size = size;
         this.state = [];
 
-        if ( state.length == 0 ) {
+        if (!state) {
             this.startState();
+        } else {
+            this.state = [...state];
         }
-
-        else {
-            this.copyState(state);
-        }
-
     }
 
     /* Clones the current board and returns a new one, with the exact same elements */
@@ -56,157 +43,88 @@ class Board {
     }
 
     /* This method initiates the board with four pieces in the middle, 2 white and 2 black */
-    startState() {
-        let totalSize = this.size*this.size;
-
-        for( let i = 0; i<totalSize; i++) {
-            this.state[i] = '0';
-        }
+    startState() {        
+        for (let i = 0; i < this.size * this.size; i++) this.state[i] = 0;
 
         let middle = this.size / 2;
         let point = new Point(middle - 1, middle - 1);
 
-        console.log("hello!");
-        this.addPiece('1', point);
-        this.addPiece('1', point.addPoint(new Point(1,1)));
-        this.addPiece('2', point.addPoint(new Point(0,1)));
-        this.addPiece('2', point.addPoint(new Point(1,0)));  
-    }
-
-    /* Copies the current possitions of the pieces in "state" 
-        and saves it in this.state*/
-    copyState(state) {
-        let totalSize = this.size*this.size;
-
-        for( let i = 0; i<totalSize; i++) {
-            this.state[i] = state[i];
-        }
-    }
-
-    /* Transforms a coordinate, like (1, 2) into someting that an array can 
-        "read". Example: (1,2) => 2*size of board + 1 */
-    decodeCoordinates(point) {
-        return point.y*this.size + point.x;
-    }
-
-    /* To be easier for the user to read and do debugging, this method transforms
-        the saved pieces in char form to string form. ONLY TO BE USED FOR 
-        DEBUGGIN, DO NOT USE IT ANYWHERE ELSE */
-    decodePieceType(ch) {
-        switch(ch) {
-            case '0':
-                return "empty";
-
-            case '1':
-                return "dark";
-
-            case '2':
-                return "light";
-
-            default:
-                return "empty";
-        }
-    }
-
-    /* Print the current board and the number of darks and whites pieces! */
-    debugBoard() {
-        if ( this.state.length == 0 ) {
-            console.log("Nothing yet!");
-        }
-
-        else {
-            console.log("dark = "+this.dark+" light = "+this.light);
-
-            let str = "";
-
-            for( let i=0; i<this.size; i++ ) {
-                for ( let j=0; j<this.size; j++ ) {
-                    let temp = this.decodePieceType(this.state[this.decodeCoordinates(new Point(j, i))]);
-
-                    if ( temp == "dark" ) { temp += " "}
-
-                    str += temp;
-
-                    if ( j!=this.size - 1 ) {
-                        str += ',';
-                    }
-                }
-                
-                str += '\n';
-            }
-
-            console.log(str);
-        }
+        this.addPiece(1, point);
+        this.addPiece(1, point.addPoint(new Point(1,1)));
+        this.addPiece(2, point.addPoint(new Point(0,1)));
+        this.addPiece(2, point.addPoint(new Point(1,0)));
     }
 
     /* This method adds a piece in possition "point". It does not check
-        if it's occupied. It does however increse the counter of light and dark pieces 
-        This method should not be used by the programmer directly!*/
+        if it's occupied. It does however increse the counter of light and dark pieces */
     addPiece(type, point) {
-        if ( type == '1' ) {
+        if (type == 1) {
             this.dark++;
-        }
-
-        else if ( type == '2' ) {
+        } else if (type == 2) {
             this.light++;
         }
 
-        this.state[this.decodeCoordinates(point)] = type;
+        this.state[point.toArrayIndex(this.size)] = type;
     }
 
-    /*  This method subs a piece in possition "point". It does not check
-        if it's occupied. It does however decreases the counter of light and dark pieces 
-        This method should not be used by the programmer directly!*/
-    rmPiece(point) {
-        let type = this.state[this.decodeCoordinates(point)];
+    /*  This method removes a piece in possition "point". It does not check
+        if it's occupied. It does however decrease the counter of light and dark pieces */
+    removePiece(point) {
+        let type = this.getPiece(point);
 
-        if ( type == '1' ) {
+        if ( type == 1 ) {
             this.dark--;
         }
 
-        else if ( type == '2' ) {
+        else if ( type == 2 ) {
             this.light--;
         }
 
-        this.state[this.decodeCoordinates(point)] = '0';
+        this.state[point.toArrayIndex(this.size)] = 0;
     }
 
-    /* As the name suggest, it inverts the type of a piece. 
+    /* As the name suggests, it inverts the type of a piece. 
         Example: dark -> light, empty -> empty, light -> dark*/
     invertType(type) {
         switch( type ) {
-            case '1':
-                return '2';
-
-            case '2':
-                return '1';
-
-            default :
-                return '0';
+            case 1:
+                return 2;
+            case 2:
+                return 1;
+            default:
+                return 0;
         }
     }
 
-    /* Get the type of the piece in possition "point". Becarefull with going out of bounds
+    /* Get the type of the piece in position "point". Be careful with going out of bounds
         because this methods does not check it */
     getPiece(point) {
-        if ( this.state.length == 0 ) {D
-            return '0'
-        }
-
+        if (this.state.length === 0) 
+            return 0
         else
-            return this.state[this.decodeCoordinates(point)];
+            return this.state[point.toArrayIndex(this.size)];
     }
 
+    getPieceName(point) {
+        switch (this.getPiece(point)) {
+            case 0:
+                return "empty";
+            case 1:
+                return "dark";
+            case 2:
+                return "light";
+        }
+    }
 
     /* Returns true if "point" is inside of the board*/
     insideBoard(point) {
-        return point.x >= 0 && point.x < this.size && point.y >= 0 && point.y < this.size ? true: false;
+        return point.x >= 0 && point.x < this.size && point.y >= 0 && point.y < this.size;
     }
 
-    /* This method will return true if the piece in possition "point" has the same 
+    /* This method will return true if the piece in position "point" has the same 
         color as "type"*/
     inRange(type, point) {
-        return this.insideBoard(point) && this.getPiece(point) == type ? true: false;
+        return this.insideBoard(point) && this.getPiece(point) == type;
     }
 
     /* This method returns an array of pieces(points) to be flipped in a GIVEN 
@@ -215,106 +133,77 @@ class Board {
         let arr = [];
         let p = point.addPoint(vec);
 
-        for ( ; this.inRange(type, p); p = p.addPoint(vec) ) {
+        for (; this.inRange(type, p); p = p.addPoint(vec)) {
             arr.push(p);
         }
 
-        if ( this.insideBoard(p) && this.getPiece(p) == this.invertType(type) ) {
+        if (this.insideBoard(p) && this.getPiece(p) == this.invertType(type)) {
             return arr;
-        }
-
-        else {
+        } else {
             return [];
         }
     }
 
     /* Get all the flippable pieces from all directions */
-    getAllflippablePieces(type, point) {
+    getAllFlippablePieces(type, point) {
         let arr = [];
-        let type1 = this.invertType(type);;
+        let type1 = this.invertType(type);
 
-        for ( let i = -1; i<=1; i++ ) {
-            if ( i != 0) {
-                arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i,0)));
-                arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(0,i)));
-                arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i,i)));
-                arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i,-i)));
-            }
-        }
+        [-1, 1].forEach(i => {
+            arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i, 0)));
+            arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(0, i)));
+            arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i, i)));
+            arr = arr.concat(this.getArrayOfPieces(type1, point, new Point(i, -i)));
+        });
 
         return arr;
     }
 
     /* Flip a piece to color "type". It does not check if it exists */
     flipPiece(point, type) {
-        this.rmPiece(point);
+        this.removePiece(point);
         this.addPiece(type, point);
-    }
-
-    /* Flips all the pieces in arrPoint to the new color "type" */
-    flipArrayOfPieces(arrPoint, type) {
-        for ( let i=0; i<arrPoint.length; i++ ) {
-            this.flipPiece(arrPoint[i], type);
-        }
     }
 
     /* Method will find and flip all possible pieces*/
     flipAllPieces(type, point) {
-        let arr = this.getAllflippablePieces(type, point);
+        let arr = this.getAllFlippablePieces(type, point);
 
-        this.flipArrayOfPieces(arr, type);
+        arr.forEach(point => this.flipPiece(point, type));
     }
 
-    /* This method returns True if a possition is valid i.e the point is inside of the board and 
-        the piece will flip some piece of the adversary. When i mean adversary, am referring 
-        the pieces that have an inverse color to "type" */
-    validPosstion(point, type) {
-        return this.insideBoard(point) && this.getPiece(point) == '0' && this.getAllflippablePieces(type, point).length != 0 ? true: false;    
+    /* This method returns True if a position is valid i.e the point is inside of the board and 
+        the piece will flip some piece of the adversary. When I mean adversary, am referring 
+        to pieces that have an inverse color to "type" */
+    validPosition(point, type) {
+        return this.insideBoard(point) && this.getPiece(point) === 0 && this.getAllFlippablePieces(type, point).length > 0;    
     }
 
     /* This method will clone the current board, then will flip the piece in "point" and flip
         all possible pieces that follows. It case of fail it will return the current board*/
     newPiece(type, point) {
-        if ( !this.insideBoard(point) ) {
-            console.log("point out of bounds");
+        switch (type) {
+            case "empty":
+                type = 0;
+                break;
+            case "dark":
+                type = 1;
+                break;
+            case "light":
+                type = 2;
+                break;
+        }
+
+        if (!this.validPosition(point, type)) {
             return this;
         }
 
         let newBoard = this.cloneBoard();
 
-        newBoard.flipPiece(point,type);
+        newBoard.flipPiece(point, type);
 
         newBoard.flipAllPieces(type, point);
 
         return newBoard;
     }
 }
-
-/* Testing: to test do: node src/js/game.js */
-
-function test() {
-    board = new Board(10);
-    board.debugBoard();
-
-    board = board.newPiece('2', new Point(4, 3));
-    board.debugBoard();
-    board = board.newPiece('1', new Point(3, 3));
-    board.debugBoard();
-    board = board.newPiece('2', new Point(4, 2));
-    board.debugBoard();
-    board = board.newPiece('2', new Point(4, 1));
-    board.debugBoard();
-    board = board.newPiece('1', new Point(4, 0));
-    board.debugBoard();
-    board = board.newPiece('2', new Point(5, 6));
-    board.debugBoard();
-    board = board.newPiece('2', new Point(2, 2));
-    board.debugBoard();
-    board = board.newPiece('2', new Point(22, 2));
-    board.debugBoard();
-
-    console.log(board.validPosstion(new Point(3,3), '1')+(new Point(3,3).tostring()));
-    console.log(board.validPosstion(new Point(4,6), '2')+(new Point(4,6).tostring()));
-}
-
-console.log(test());
